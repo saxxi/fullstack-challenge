78ceda31d8c2eb79baf29d01a31892ce0da1a485
Saxxi <a.saxena.email@gmail.com>
2017-09-25 01:20:37 +0200

    bug fix on upvote

--- app/use_cases/up_vote.rb
+++ app/use_cases/up_vote.rb
@@ -14,8 +14,10 @@ class UpVote
   attr_reader :comic_id
 
   def create_or_increase
-    comic = ComicVote.upvote(comic_id: comic_id)
-    comic.tap do |success|
+    # http://guidelines.plataformatec.com.br/ruby.html read 'tap'
+    comic_vote = ComicVote.upvote(comic_id: comic_id)
+    success = comic_vote.save
+    comic_vote.tap do |comic_vote|
       errors.add(:base, 'Vote failed to save') unless success
     end
   end
